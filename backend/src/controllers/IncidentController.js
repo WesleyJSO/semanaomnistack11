767f272e61f4436fa571fa670348f88d5157bd15
Wesley <wesley.silva@muralis.com.br>
2020-03-28 23:15:42 -0300

    add: mobile api calls to the backend

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -4,22 +4,20 @@ module.exports = {
   async index(request, response) {
     const { page = 1 } = request.query
 
-    const [count] = await connection(`incidents`).count()
     
     const incidents = await connection(`incidents`)
-    .join(`ongs`, `ong_id`, `=`, `incidents.ong_id`)
-    .limit(5)
-    .offset((page - 1) * 5)
-    .select([
-      `incidents.*`, 
-      `ongs.name`, 
-      `ongs.email`, 
-      `ongs.whatsapp`, 
-      `ongs.city`, 
-      `ongs.uf`
-    ])
-    
-    response.header(`X-Total-Count`, count[`count(*)`])
+      .join(`ongs`, `ongs.id`, `=`, `incidents.ong_id`)
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select([
+        `incidents.*`, 
+        `ongs.name`, 
+        `ongs.email`, 
+        `ongs.whatsapp`, 
+        `ongs.city`, 
+        `ongs.uf`
+      ])
+    response.header(`X-Total-Count`, incidents.length)
     return response.json(incidents)
   },
 
@@ -40,7 +38,6 @@ module.exports = {
   async delete(request, response) {
     const { id } = request.params
     const ong_id = request.headers.authorization
-    console.log(ong_id)
     const incident = await connection(`incidents`)
       .where(`id`, id)
       .select(`ong_id`)
--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Feather } from '@expo/vector-icons'
 import { Linking, Text, View, Image, TouchableOpacity } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import * as MailComposer from 'expo-mail-composer'
 
 import styles from './styles'
@@ -10,8 +10,10 @@ import logoImg from '../../assets/logo.png'
 export default function Details() {
 
   const navigation = useNavigation()
+  const route = useRoute()
+  const incident = route.params.incident
 
-  const message = 'teste mensagem'
+  const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso ${incident.title} com o valor de ${Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL'}).format(incident.value)}`
 
   function navigateBack() {
     navigation.goBack()
@@ -19,14 +21,14 @@ export default function Details() {
 
   function sendMail() {
     MailComposer.composeAsync({
-      subject: 'teste:teste',
+      subject: `${incident.name}: incidente ${incident.title}`,
       recipients: ['josewso55@gmail.com'],
       body: message,
     })
   }
 
   function sendWhatsapp() {
-    Linking.openURL(`whatsapp://send?phone=5511974221186&text=${message}`)
+    Linking.openURL(`whatsapp://send?phone=${incident.whatsapp}&text=${message}`)
   }
 
   return (
@@ -42,13 +44,17 @@ export default function Details() {
       <View style={styles.incident}>
         
         <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
-        <Text style={styles.incidentValue}>APAD</Text>
+        <Text style={styles.incidentValue}>
+          {incident.name} de {incident.city}-{incident.uf}
+        </Text>
         
         <Text style={styles.incidentProperty}>CASO:</Text>
-        <Text style={styles.incidentValue}>APAD</Text>
+        <Text style={styles.incidentValue}>{incident.description}</Text>
         
         <Text style={styles.incidentProperty}>VALOR:</Text>
-        <Text style={styles.incidentValue}>12</Text>
+        <Text style={styles.incidentValue}>
+          {Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL'}).format(incident.value)}
+        </Text>
 
         </View>
 
